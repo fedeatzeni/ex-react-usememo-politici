31a9e3fd55c056bc7592a24c004ca0f3c4991016
Fede <fede.atzeni372@gmail.com>
2025-06-05 12:05:16 +0200

    feat milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 
 function App() {
 
 	const url = "http://localhost:3333"
 
 	const [data, setData] = useState([])
+	const [search, setSearch] = useState("")
+
 
 	const loadData = async () => {
 		const res = await fetch(`${url}/politicians`);
@@ -16,14 +18,20 @@ function App() {
 		loadData();
 	}, []);
 
-	console.log(data);
+	const filtredData = useMemo(() => data.filter(el => el.name.toLowerCase().includes(search.toLowerCase()) ||
+		el.biography.toLowerCase().includes(search.toLowerCase())), [data, search])
+
+	// console.log(data);
+	// console.log(search);
 
 	return (
 
 		<>
 			<h2>Lista Politici</h2>
+			<span>Cerca:</span>
+			<input type="text" value={search} onChange={e => setSearch(e.target.value)} />
 			<ul>
-				{data.map(p => (
+				{filtredData.map(p => (
 					<li key={p.id}>
 						<p>{`Nome: ${p.name}`}</p>
 						<p>{`Posizione: ${p.position}`}</p>
